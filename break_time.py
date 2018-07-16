7bd53a1c7c668223f161220674393c372ecf763a
Liz Khuu <khuu.liz@gmail.com>
2018-07-16 08:25:30 -0400

    initial file commit

---
+++ break_time.py
@@ -0,0 +1,30 @@
+# ======== TAKE A BREAK =========
+# function assumes that the user is not already listening to music while doing work
+
+import time
+import webbrowser
+import random
+
+def take_a_break(amount_of_breaks):
+    total_breaks = amount_of_breaks 
+    break_count = 0
+
+    print(start_message)
+
+    while break_count < total_breaks:
+        time.sleep(time_in_seconds)
+        webbrowser.open(break_music)
+
+        break_count += 1
+
+        if break_count == (total_breaks - 1):
+            print(motivational_statement)
+
+# === SET THE VARIABLE VALUES BELOW ===
+start_message = "Hi! This program started on " + time.ctime()
+time_in_seconds = (2 * (60 * 60))
+break_music = "https://www.youtube.com/watch?v=vKikM_W_Fpk"
+motivational_statement = "We kicked ass today...and danced! Good job! :)"
+
+# === CALL UP THE FUNCTION ===
+take_a_break(3)
