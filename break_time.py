995537895e3e74879d47d07670e2e9791a8f456f
Liz Khuu <khuu.liz@gmail.com>
2018-07-26 00:28:41 -0400

    changed links from a dictionary to a list

--- break_time.py
+++ break_time.py
@@ -1,5 +1,6 @@
 # ======== TAKE A BREAK =========
 # function assumes that the user is not already listening to music while doing work
+# iterates through a list and randomly picks a link 
 
 import time
 import webbrowser
@@ -12,28 +13,30 @@ def take_a_break(amount_of_breaks):
     print(start_message)
 
     while break_count < total_breaks:
-        break_music = links[random.randint(1,3)]
+        break_music = random.choice(links)
         time.sleep(time_in_seconds)
         webbrowser.open(break_music)
-
+        
         break_count += 1
 
         if break_count == (total_breaks - 1):
             print(motivational_statement)
 
 # === SET THE VARIABLE VALUES BELOW ===
-start_message = "Hi! This program started on " + time.ctime()
+start_message = "Hi! This program started on: " + time.ctime()
 time_in_seconds = (2 * (60 * 60))
 
-links = {
-    1: "https://www.youtube.com/watch?v=vKikM_W_Fpk",
-    2: "https://www.youtube.com/watch?v=x0-v2k14I-Q",
-    3: "https://www.youtube.com/watch?v=OHz1fCSHTLA",
-    4: "https://www.youtube.com/watch?v=MPa-kWJhnvA",
-    5: "https://www.youtube.com/watch?v=091nrE66kpg"
-}
+links = [
+    "https://www.youtube.com/watch?v=vKikM_W_Fpk", 
+    "https://www.youtube.com/watch?v=x0-v2k14I-Q",
+    "https://www.youtube.com/watch?v=OHz1fCSHTLA", 
+    "https://www.youtube.com/watch?v=MPa-kWJhnvA",
+    "https://www.youtube.com/watch?v=091nrE66kpg"
+]
+
+links_length = len(links)
 
 motivational_statement = "We kicked ass today...and danced! Good job! :)"
 
 # === CALL UP THE FUNCTION ===
-take_a_break(3)
+take_a_break(4)
